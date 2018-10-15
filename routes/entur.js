9637398c38522b033df40937202847baecda29e9
Tom-Olav Bøyum <boyum90@gmail.com>
2018-10-15 13:26:35 +0200

    Update entur.js

--- routes/entur.js
+++ routes/entur.js
@@ -4,7 +4,6 @@ var getClosestStops = require('../services/entur');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  console.log('here');
   return getClosestStops(req.query).then(data => res.send(data))
 });
 
